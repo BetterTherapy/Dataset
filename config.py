f1ef6b8b6d07f85fc706f6c2db54bc5436059d1c
Nishan Bhandari <bnishan18@gmail.com>
2025-08-30 22:49:44 +0545

    fix: parse vram and cpu as string

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ class Settings(BaseSettings):
 
     model_name: str = Field(default="meta-llama/Llama-3.1-8B-Instruct")
     offload_to_cpu: bool = Field(default=True)
-    vram_in_GiB: int = Field(default=16)
-    cpu_in_GiB: int = Field(default=20)
+    vram_in_GiB: str = Field(default=16)
+    cpu_in_GiB: str = Field(default=20)
     data_count: int = Field(default=1000)
     database_url: str = Field(default="")
 
