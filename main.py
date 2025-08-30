811d5e4518a986831fd9e1eccec65044556cc018
Nishan Bhandari <bnishan18@gmail.com>
2025-08-31 01:21:15 +0545

    fix: error handling

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from hf_model.generator import generate_response
 from hf_model.init_model import setup_model
 import logging
 from rich.logging import RichHandler
+import time
 
 # Configure logging with rich
 logging.basicConfig(
@@ -17,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 def main():
     model, tokenizer = setup_model(config.model_name)
-    prompt = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>  
+    system_prompt = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>  
     You are a compassionate mental health assistant.  
     Generate both a mental health question and its empathetic answer.  
     Respond **only** with a VALID JSON object that:  
@@ -29,7 +30,12 @@ def main():
     <|start_header_id|>assistant<|end_header_id|>{ 
     """
     for i in range(config.data_count):
-        base_data = generate_response(prompt, model, tokenizer)
+        start_time = time.time()
+        try:
+            base_data = generate_response(system_prompt, model, tokenizer)
+        except Exception as e:
+            logger.error(f"Error generating response: {e}")
+            continue
         prompt = base_data.get("question", None)
         base_response = base_data.get("answer", None)
         if not prompt or not base_response:
@@ -40,7 +46,11 @@ def main():
             response=base_response,
         )
         add_base_prompt_response(base_prompt_response)
-        logger.info(f"Successfully added {i + 1} base prompt response ")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logger.info(
+            f"Successfully added {i + 1} base prompt response in {elapsed_time:.2f} seconds."
+        )
 
 
 if __name__ == "__main__":
